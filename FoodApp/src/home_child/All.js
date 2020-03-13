8e603d97b33c3123a7e793d258c6b2a54739e83d
MrWilllian <willian.dev10@gmail.com>
2020-03-13 09:53:39 -0300

    refactor: add All page changes

--- FoodApp/src/home_child/All.js
+++ FoodApp/src/home_child/All.js
@@ -5,47 +5,53 @@ import {
   StyleSheet,
   FlatList,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+var data = [
+  {
+    name:'Stewed Mushrooms',
+    image: require("../asset/namkho.jpg"),
+    rating: 3,
+    price: "$12"
+  },
+  {
+    name:'Jackfruit Fried',
+    image: require("../asset/mitkho.jpg"),
+    rating: 5,
+    price: "$15"
+  },
+  {
+    name:'Noodles',
+    image: require("../asset/hutieu.jpg"),
+    rating: 4,
+    price: "$20"
+  },
+  {
+    name:'Beef',
+    image: require("../asset/cuonlalot.jpg"),
+    rating: 2,
+    price: "$12"
+  },
+  {
+    name:'Salad dressing',
+    image: require("../asset/cuondiep.jpg"),
+    rating: 5,
+    price: "$13"
+  },
+];
 
 export default class All extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      data: [
-        {
-          name:'Stewed Mushrooms',
-          image: require("../asset/namkho.jpg"),
-          rating: 3,
-          price: "$12"
-        },
-        {
-          name:'Jackfruit Fried',
-          image: require("../asset/mitkho.jpg"),
-          rating: 5,
-          price: "$15"
-        },
-        {
-          name:'Noodles',
-          image: require("../asset/hutieu.jpg"),
-          rating: 4,
-          price: "$20"
-        },
-        {
-          name:'Beef',
-          image: require("../asset/cuonlalot.jpg"),
-          rating: 2,
-          price: "$12"
-        },
-        {
-          name:'Salad dressing',
-          image: require("../asset/cuondiep.jpg"),
-          rating: 5,
-          price: "$13"
-        },
-      ]
+      data: data,
+      data_temp: data,
+      search: '',
     }
   }
 
@@ -67,7 +73,7 @@ export default class All extends React.Component {
       <LinearGradient
         colors={['#009245', '#8cc631']}
         start={{x:0, y:1}} end={{x:1, y:0}}
-        tyle={styles.item}>
+        style={styles.item}>
 
         <View style={styles.image_container}>
           <Image source={item.image} style={styles.image} />
@@ -109,9 +115,38 @@ export default class All extends React.Component {
     );
   }
 
+  _search(text) {
+    let data = [];
+    this.state.data_temp.map(function(value) {
+      if (value.name.indexOf(text) > -1) {
+        data.push(value);
+      }
+    });
+    this.setState({
+      data: data,
+      search: text,
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.section}>
+          <TextInput 
+            placeholder="Search..." 
+            style={{flex: 1, marginLeft: 10}}
+            value={this.state.search}
+            onChangeText={(text) => this._search(text)}  
+            />
+            <TouchableOpacity
+              onPress={() => this._search('')}
+              style={{paddingHorizontal: 10}}>
+              <Ionicons
+                name="ios-close"
+                color="gray"
+                size={20} />
+            </TouchableOpacity>
+        </View>
         <View style={styles.flatList}>
           <FlatList
             data={this.state.data}
@@ -163,7 +198,7 @@ var styles = StyleSheet.create({
   name: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 10
+    fontSize: 18
   },
   rating: {
     marginTop: 5,
@@ -184,11 +219,20 @@ var styles = StyleSheet.create({
   price: {
     backgroundColor: 'white',
     paddingVertical: 5,
-    paddingHorizontal: 5,
+    paddingHorizontal: 15,
     borderRadius: 50
   },
   textPrice: {
     color: 'green',
     fontWeight: 'bold',
-  }
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    backgroundColor: '#f2f2f2',
+    marginTop: 10
+  },
 });
